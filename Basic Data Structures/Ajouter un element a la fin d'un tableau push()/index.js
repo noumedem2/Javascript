45caca1dcbffbe3080bfd0176a6fe67c4a3d1d79
noumedem <noumedemtamiflorian@gmail.com>
2021-04-05 00:25:30 +0200

    Cours : Ajouter un element a la fin d'un tableau push()

---
+++ Basic Data Structures/Ajouter un element a la fin d'un tableau push()/index.js
@@ -0,0 +1,7 @@
+/**
+ * Ajouter un element a la fin d'un tableau
+ * 
+ */
+arr.push('element ajouter a la fin')
+ // console.log(arr);
+ // [1, "2", false, "element ajoute"]
--- Basic Data Structures/index.js
+++ Basic Data Structures/index.js
@@ -9,14 +9,6 @@
  */
 
 
-
-/**
- * Ajouter un element a la fin d'un tableau
- * 
- */
-arr.push('element ajouter a la fin')
-// console.log(arr);
-// [1, "2", false, "element ajoute"]
 /**
  * Ajouter un element au debut de tableau
  */
