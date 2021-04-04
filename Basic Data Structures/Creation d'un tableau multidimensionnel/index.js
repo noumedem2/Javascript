6fd44b2f8cce36a5d9e3c7c35fea06b86855ea52
noumedem <noumedemtamiflorian@gmail.com>
2021-04-05 00:14:10 +0200

    Cours : Creation d'un tableau multidimensionnel

---
+++ Basic Data Structures/Creation d'un tableau multidimensionnel/index.js
@@ -0,0 +1,29 @@
+/**
+ * Tableau complexe ou tableau multidimensionnel
+ */
+let complexArray = [
+    [
+        {
+            one: 1,
+            two: 2
+        },
+        {
+            three: 3,
+            four: 4
+        }
+    ],
+    [
+        {
+            a: "a",
+            b: "b"
+        },
+        {
+            c: "c",
+            d: "d"
+        }
+    ]
+];
+let yourArray;
+yourArray = [
+    [], true, 'Hello', 1, []
+]
