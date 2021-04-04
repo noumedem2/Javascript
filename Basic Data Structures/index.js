cb23eb4ddb9a7128fd860d6d5d5dec49e68b93da
noumedem <noumedemtamiflorian@gmail.com>
2021-04-05 00:31:33 +0200

    Cours : Suprimer un element a la fin pop()

--- Basic Data Structures/index.js
+++ Basic Data Structures/index.js
@@ -8,13 +8,7 @@
  * spread
  */
 
-/**
- * Suprimer un element a la fin ou au debut d'un tableau
- */
-let greetings = ['whats up?', 'hello', 'see ya!'];
-greetings.pop();
-// console.log(greetings);
-// ["whats up?", "hello"]
+
 greetings.shift()
 // console.log(greetings);
 // ["hello"]
