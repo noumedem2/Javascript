1b55d5f1b64209f333c5b05070844590451f70a8
noumedem <noumedemtamiflorian@gmail.com>
2021-10-04 11:24:26 +0100

    Appariement ADN

---
+++ Intermediate Algorithm Scripting/Appariement ADN/index.js
@@ -0,0 +1,20 @@
+var pairElement = function (str) {
+    var tabs = str.split('');
+    return tabs.reduce(function (acc, previous) {
+        switch (previous) {
+            case "A":
+                return acc.concat([["A", "T"]]);
+            case "T":
+                return acc.concat([["T", "A"]]);
+            case "C":
+                return acc.concat([["C", "G"]]);
+            case "G":
+                return acc.concat([["G", "C"]]);
+        }
+    }, []);
+};
+pairElement("GCAG");
+console.log(pairElement("GCAG"));
+console.log(pairElement("ATCGA"));
+console.log(pairElement("TTGAG"));
+console.log(pairElement("CTCTA"));
